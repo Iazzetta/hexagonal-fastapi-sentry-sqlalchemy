5847c3ca7d2f3b9b489a916a3a1015b0eb0a5f26
Guilherme Iazzetta <guilherme.iazzetta@gmail.com>
2023-01-06 18:18:35 -0300

    remove database session from interface

--- src/core/domain/user/database_interface.py
+++ src/core/domain/user/database_interface.py
@@ -2,14 +2,12 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Optional
 
-from sqlalchemy.orm import Session
-from src.datalayer.database.postgres.engine import get_db
 from src.core.domain.user.user import User
 
 @dataclass
 class UserDatabaseInterface(ABC):
 
-    database: Session = next(get_db())
+    database: any
 
     @abstractmethod
     def create(self, name: str, email: str, password: str) -> User:
--- src/presentation/factories/user.py
+++ src/presentation/factories/user.py
@@ -1,4 +1,7 @@
 from src.core.domain.user.service import UserAdapter
+from sqlalchemy.orm import Session
+from src.datalayer.database.postgres.engine import get_db
 
 def make_user_controller():
-    return UserAdapter()
+    database: Session = next(get_db())
+    return UserAdapter(database)
